6702fe46371afe1e39af96302158cc405e52ddd8
iosifakis21 <iosifktenidis@gmail.com>
2026-06-25 20:10:47 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ const productData: Record<TabKey, ProductEntry> = {
     ],
     url: 'https://constantinebeauty.gr/products/aether-acne-cream',
     image: 'https://6q04fcrv8e.ufs.sh/f/oRxwcUqHTaVC8jzi4JuBuzK2lRNEWasfkOcy4p6g0TMqDV1Z',
-    imagePosition: 'center 20%',
+    imagePosition: 'center 65%',
   },
   wrinkles: {
     name: 'HYDRANEA',
@@ -100,7 +100,7 @@ const productData: Record<TabKey, ProductEntry> = {
     ],
     url: 'https://constantinebeauty.gr/products/elisheva-dark-spots-cream',
     image: 'https://6q04fcrv8e.ufs.sh/f/oRxwcUqHTaVCIaaLfQpIiabJK94o2U0QOXxNtR8HSkZghnBD',
-    imagePosition: 'center 20%',
+    imagePosition: 'center 65%',
   },
 };
 
